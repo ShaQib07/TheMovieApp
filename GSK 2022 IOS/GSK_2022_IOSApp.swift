c4b7233b3f59878985b7cf2f12a0820419289d6d
golamshakib <sHaQiB07>
2022-09-15 10:19:01 +0600

    Initial Commit

---
+++ GSK 2022 IOS/GSK_2022_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GSK_2022_IOSApp.swift
+//  GSK 2022 IOS
+//
+//  Created by Golam Shakib Khan on 15/9/22.
+//
+
+import SwiftUI
+
+@main
+struct GSK_2022_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
